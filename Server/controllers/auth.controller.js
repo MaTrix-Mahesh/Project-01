f3e60c195fe771d9af9b1bd64dbbfc4034412563
mahesh sahu <shamahesh615@gmail.com>
2026-03-12 08:48:35 +0530

    initial commit

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -1,3 +1,24 @@
-module.exports.register = function(req,res){
-    res.send("controller run successfully----")
+const User = require("../models/user");
+
+module.exports.register = async function(req,res){
+
+    const {name , email,password} = req.body;
+
+    try{
+        if (!name || !email || !password) {
+            return res.status(200).json({
+                message:"all field are required"
+            })
+        }
+
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({
+                message : 'user already exist '
+            })
+        }
+    }catch(err){
+        console.log(err);
+    }
 }
--- Server/routes/auth.routes.js
+++ Server/routes/auth.routes.js
@@ -2,6 +2,6 @@ let express = require("express");
 const { register } = require("../controllers/auth.controller");
 const router = express.Router();
 
-router.get("/",register)
+router.post("/",register)
 
 module.exports = router;
